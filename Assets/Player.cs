9dc09c5ccb330a8ce3476087780d306d7df30ffe
8ctopotamus <joshnaylor88@gmail.com>
2017-10-06 17:55:30 -0500

    player movement

---
+++ Assets/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public float speed = 4f;
+
+	public Rigidbody2D rb;
+
+	private float movement = 0;
+
+	void Update () {
+		movement = Input.GetAxisRaw("Horizontal") * speed;
+	}
+
+	void FixedUpdate () {
+		rb.MovePosition(rb.position + new Vector2(movement * Time.fixedDeltaTime, 0f));
+	}
+}
